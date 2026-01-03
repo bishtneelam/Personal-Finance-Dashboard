5d1383643955edf6a1761ffd2417a59f85c47060
Neelam Bisht <bishtneelam1212@gmail.com>
2026-01-03 06:36:00 +0530

    Category and Type Filtering UI added

--- src/features/transactionHistory/hooks/useSearchFilter.ts
+++ src/features/transactionHistory/hooks/useSearchFilter.ts
@@ -2,19 +2,39 @@ import { useState } from "react";
 
 export function useSearchFilter() {
     const [showFilters, setShowFilters] = useState(false);
-    const [type, setType] = useState("all");
-    const [category, setCategory] = useState("all");
+    const [filterType, setFilterType] = useState("all");
+    const [selectedCategories, setSelectedCategories] = useState<String[]>([]);
     const [searchQuery, setSearchQuery] = useState("");
+    const activeFiltersCount = (filterType === "all" ? 0 : 1) + selectedCategories.length;
 
     const handleFilterClick = () => {
         setShowFilters(prev => !prev)
     }
+    const toggleCategories = (categoryId: string) => {
+        setSelectedCategories(prev => (
+            prev.includes(categoryId)
+            ? prev.filter(id => id!==categoryId)
+            : [...prev, categoryId]
+        ))
+    }
+    const clearCategories = () => {
+        setSelectedCategories([]);
+    }
+    const clearAllFilters = () => {
+        clearCategories();
+        setFilterType("all")
+    }
     return {
         showFilters,
-        type,
-        category,
+        filterType,
+        selectedCategories,
         searchQuery,
+        activeFiltersCount,
+        setFilterType,
         setSearchQuery,
-        handleFilterClick
+        handleFilterClick,
+        toggleCategories,
+        clearCategories,
+        clearAllFilters,
     }
 }
--- src/features/transactionHistory/ui/SearchFilter.jsx
+++ src/features/transactionHistory/ui/SearchFilter.jsx
@@ -1,9 +1,22 @@
-import { Filter, Search } from "lucide-react";
+import { ArrowDownCircle, ArrowUpCircle, Filter, Search, X } from "lucide-react";
 import styles from "../styles/TransactionHistory.module.css";
 import { useSearchFilter } from "../hooks/useSearchFilter";
+import { CATEGORIES } from "../../../data/categories";
 export function SearchFilter() {
-  const { searchQuery, showFilters, setSearchQuery, handleFilterClick } =
-    useSearchFilter();
+  const {
+    searchQuery,
+    showFilters,
+    filterType,
+    selectedCategories,
+    activeFiltersCount,
+    setSearchQuery,
+    handleFilterClick,
+    setFilterType,
+    toggleCategories,
+    clearCategories,
+    clearAllFilters,
+  } = useSearchFilter();
+  console.log("Active", activeFiltersCount)
   return (
     <>
       <div className={styles.searchContainer}>
@@ -29,17 +42,95 @@ export function SearchFilter() {
       </div>
 
       {/* Filter Panel */}
-      {
-        showFilters && (
-            <div classname={styles.filterPanel}>
-                <div className={styles.filterSection}>
-                    <div className={styles.filterSectionHeader}>
+      {showFilters && (
+        <div className={styles.filtersPanel}>
+          <div className={styles.filterSection}>
+            <p className={styles.header}>Transaction Type</p>
+            {filterType !== "all" && (
+              <button
+                className={styles.clearButton}
+                onClick={() => setFilterType("all")}
+              >
+                Clear
+              </button>
+            )}
+          </div>
+          <div className={styles.filterHeader}>
+            <button
+              className={`${styles.typeButton} ${
+                filterType === "all" ? styles.filterButtonActive : ""
+              }`}
+              onClick={() => setFilterType("all")}
+            >
+              All
+            </button>
+            <button
+              className={`${styles.typeButton} ${
+                filterType === "credit" ? styles.creditBtnActive : {}
+              }`}
+              onClick={() => setFilterType("credit")}
+            >
+              <ArrowUpCircle size={16} /> Credit Only
+            </button>
+            <button
+              className={`${styles.typeButton} ${
+                filterType === "debit" ? styles.debitBtnActive : {}
+              }`}
+              onClick={() => setFilterType("debit")}
+            >
+              <ArrowDownCircle size={16} /> Debit Only
+            </button>
+          </div>
 
-                    </div>
-                </div>
-            </div>
-        )
-      }
+          <div className={styles.filterSection}>
+            <p className={styles.header}>Categories</p>
+            {selectedCategories.length > 0 && (
+              <button className={styles.clearButton} onClick={clearCategories}>
+                Clear
+              </button>
+            )}
+          </div>
+
+          <div className={styles.categoryFilters}>
+            {Object.entries(CATEGORIES).map(([key, value]) => {
+              const CategoryIcon = value.icon;
+              return (
+                <button
+                  key={key}
+                  className={`${styles.categorySection}
+                  ${
+                    selectedCategories.includes(key)
+                      ? styles.categoryFilterButtonActive
+                      : ""
+                  }`}
+                  style={
+                    selectedCategories.includes(key)
+                      ? {
+                          borderColor: value.color,
+                          backgroundColor: `${value.color}15`,
+                        }
+                      : {}
+                  }
+                  onClick={() => toggleCategories(key)}
+                >
+                  <div
+                    className={styles.categoryIcon}
+                    style={{ color: "#fff", backgroundColor: value.color }}
+                  >
+                    <CategoryIcon size={20} />
+                  </div>
+                  <span>{value.name}</span>
+                </button>
+              );
+            })}
+          </div>
+
+          {activeFiltersCount > 0 && <button className={styles.clearAllFilters} onClick={clearAllFilters}>
+            <X size={16} />
+            <span>{`Clear All Filters (${activeFiltersCount})`}</span>
+            </button>}
+        </div>
+      )}
     </>
   );
 }
